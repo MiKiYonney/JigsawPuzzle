65a543ea988c958549ebdc5e2f0fb2fb7261fc24
beforeload <fe.daniel91@gmail.com>
2014-04-05 01:22:49 +0800

    init the repo

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('puzzleApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
